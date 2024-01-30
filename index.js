534e2405acf0e1112e6f0e2aa02d21c9b9f180f7
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-01-30 16:53:44 -0500

    Finish part 3.2, get info date

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
